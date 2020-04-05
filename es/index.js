ff8b223edf03baed391b9a42355ade0a49a6490e
overlookmotel <theoverlookmotel@gmail.com>
2020-04-05 14:59:46 +0100

    ESM export production/dev build depending on `NODE_ENV`

--- es/index.js
+++ es/index.js
@@ -1,8 +1,39 @@
 /* --------------------
  * {{name}} module
  * ESM Entry point
+ * Re-export in tree-shakable form, choosing dev or prod build based on NODE_ENV.
  * ------------------*/
+
+// Imports
+import {{nameCamel}}Prod from '../dist/esm/{{name}}.min.js';
+import {{nameCamel}}Dev from '../dist/esm/{{name}}.js';
 
 // Exports
 
-export * from '../dist/esm/{{name}}.js';
+export default process.env.NODE_ENV === 'production'
+	? {{nameCamel}}Prod
+	: {{nameCamel}}Dev;
+
+/*
+// Alternative for named exports
+
+// Imports
+import {
+	method1 as method1Prod,
+	method2 as method2Prod
+} from '../dist/esm/{{name}}.min.js';
+
+import {
+	method1 as method1Dev,
+	method2 as method2Dev
+} from '../dist/esm/{{name}}.js';
+
+// Exports
+
+export const method1 = process.env.NODE_ENV === 'production'
+	? method1Prod
+	: method1Dev;
+export const method2 = process.env.NODE_ENV === 'production'
+	? method2Prod
+	: method2Dev;
+*/
--- src/index.js
+++ src/index.js
@@ -5,4 +5,4 @@
 
 // Exports
 
-export {};
+export default function() {};
